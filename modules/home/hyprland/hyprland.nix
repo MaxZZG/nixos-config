f4f8ef51439d1b22cd0236b812d74ba98f6e6070
Max <zzgdar@163.com>
2024-11-05 22:15:54 +0800

    Test fonts

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -1,7 +1,17 @@
 { inputs, pkgs, ... }:
 {
-  programs.kitty.enable = true;
-  systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
+  home.packages = with pkgs; [
+    nerdfonts
+  ];
+
+  programs.kitty = {
+    enable = true;
+    font = {
+        name = "Recursive Mono Nerd Font";
+        size = 12;
+      };
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
 
