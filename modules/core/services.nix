b22917f1c5d0633a4e3c282dd3894c64050166b7
Max <zzgdar@163.com>
2024-11-07 20:57:00 +0800

    auto generate ssh key

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,7 +1,18 @@
 { ... }: 
 {
   services = {
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+      hostKeys = [
+        {
+          bits = 4096;
+          path = "/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+          comment = "zzgdar@163.com";
+        }
+      ];
+    }
+    
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
