55cc6696c4410d85d3b918446d73060f1147a153
Max <zzgdar@163.com>
2025-03-02 21:37:19 +0800

    update hyprlock

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -5,13 +5,5 @@
 
     userName = "Max";
     userEmail = "zzgdar@163.com";
-
-    extraConfig = {
-      url = {
-        "https://gh-proxy.icu/https://github.com/" = {
-          insteadOf = "https://github.com/";
-        };
-      };
-    };
   };
 }
