9d277d9759c9f8db52cfa78c14db3bf9a5191c64
Max <zzgdar@163.com>
2026-08-15 16:02:00 +0800

    update

--- modules/features/git/home.nix
+++ modules/features/git/home.nix
@@ -3,7 +3,9 @@
   # git 用户侧：用户配置
   programs.git = {
     enable = true;
-    userName = "Max";
-    userEmail = "zzgdar@163.com";
+    settings = {
+      user.name = "Max";
+      user.email = "zzgdar@163.com";
+    };
   };
 }
